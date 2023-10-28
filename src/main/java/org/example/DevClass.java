0efc37b093ebbbec0dca5f876b8b75a997562077
Ballaleshwar <53635360+Ballaleshwar@users.noreply.github.com>
2023-10-28 16:45:04 +0530

    Update DevClass.java (#2)

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -3,7 +3,7 @@ package org.example;
 public class DevClass {
 
     public static void main(String[] args) {
-        System.out.println("in DevClass - otherGitAccount");
+        System.out.println("in DevClass - otherGitAccount + 3415");
 
         //changes done by bsg.
 
