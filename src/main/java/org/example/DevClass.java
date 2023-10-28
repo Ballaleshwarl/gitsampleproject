ea2f24e064a6ebcfefdc8043d56796402afe305c
Ballaleshwar <53635360+Ballaleshwar@users.noreply.github.com>
2023-10-28 16:51:29 +0530

    Update DevClass.java
    updated

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -5,6 +5,7 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("in DevClass - otherGitAccount + 3415");
 
+        System.out.pritnln("another commit for merge and squash!!");
         //changes done by bsg.
 
         //changes done by otherGitAccount
